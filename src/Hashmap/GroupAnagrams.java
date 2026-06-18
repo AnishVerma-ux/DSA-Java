764ba767b984735ab655341f6ea21e714b82b6d1
anishverma <anishverma631@gmail.com>
2026-06-18 23:59:27 +0530

    leetcode 49 Group Anagrams

---
+++ src/Hashmap/GroupAnagrams.java
@@ -0,0 +1,31 @@
+package Hashmap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+
+
+        for (String str : strs) {
+
+            char[] charArray = str.toCharArray();
+
+
+            Arrays.sort(charArray);
+
+
+            String sortedKey = String.valueOf(charArray);
+
+
+            anagramGroups.computeIfAbsent(sortedKey, key -> new ArrayList<>()).add(str);
+        }
+
+
+        return new ArrayList<>(anagramGroups.values());
+    }
+}
