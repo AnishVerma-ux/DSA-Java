30f88a09b7b2d47bf3aad4db9514ea009a3395f0
anishverma <anishverma631@gmail.com>
2026-06-19 23:12:14 +0530

    leetcode 1732 : highest altitude

---
+++ src/Arrays/HighestAltitude.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class HighestAltitude {
+
+    public static int largestAltitude(int[] gain) {
+
+        int currentAltitude = 0;
+        int maxAltitude = 0;
+
+        for (int g : gain) {
+            currentAltitude += g;
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+
+        return maxAltitude;
+    }
+
+    public static void main(String[] args) {
+
+        int[] gain = {-5, 1, 5, 0, -7};
+
+        System.out.println(largestAltitude(gain));
+    }
+}
