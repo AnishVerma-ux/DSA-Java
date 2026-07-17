51af6f7202f317b5bcb717b5d9e2d20e97380015
Anish Verma <anishverma631@gmail.com>
2026-07-18 01:46:38 +0530

    Time: 1 ms (97.56%), Space: 42.6 MB (43.09%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        int l = 0;
+        int h = x;
+        int ans = 0;
+
+        while (l <= h) {
+
+            int mid = l + (h - l) / 2;
+
+            long square = (long) mid * mid;
+
+            if (square <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
